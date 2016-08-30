c76b1944e9e39fceba780361e9b8fcdaf3a9f618
Giuseppe Loianno <loiannog@gmail.com>
2016-08-30 18:57:54 -0400

    adding more output desired values

--- src/state_control.cpp
+++ src/state_control.cpp
@@ -208,7 +208,7 @@ static void nanokontrol_cb(const sensor_msgs::Joy::ConstPtr &msg)
 
 static void odom_cb(const nav_msgs::Odometry::ConstPtr &msg)
 {
-  if(state_ != TRAJ){
+
       odom_des.header.stamp = ros::Time::now();
       odom_des.pose.pose.position.x = traj_goal.position.x;
       odom_des.pose.pose.position.y = traj_goal.position.y;
@@ -221,8 +221,7 @@ static void odom_cb(const nav_msgs::Odometry::ConstPtr &msg)
       odom_des.twist.twist.linear.y = traj_goal.velocity.y;
       odom_des.twist.twist.linear.z = traj_goal.velocity.z;
       des_odom_pub.publish(odom_des);
-      des_odom_pub.publish(odom_des);  
-  }
+ 
   // If we are currently executing a trajectory, update the setpoint
   if (state_ == TRAJ)
   {
@@ -260,18 +259,6 @@ static void odom_cb(const nav_msgs::Odometry::ConstPtr &msg)
       traj_num_msg.data = traj_num_;
       pub_traj_num_.publish(traj_num_msg);
       //publish the desired trajectory
-      
-      odom_des.pose.pose.position.x = traj_goal.position.x;
-      odom_des.pose.pose.position.y = traj_goal.position.y;
-      odom_des.pose.pose.position.z = traj_goal.position.z;
-      odom_des.pose.pose.orientation.w = traj_goal.yaw;//save just the yaw in the scalar part of the quaternion
-      odom_des.pose.pose.orientation.x = 0;
-      odom_des.pose.pose.orientation.y = 0;
-      odom_des.pose.pose.orientation.z = 0;
-      odom_des.twist.twist.linear.x = traj_goal.velocity.x;
-      odom_des.twist.twist.linear.y = traj_goal.velocity.y;
-      odom_des.twist.twist.linear.z = traj_goal.velocity.z;
-      des_odom_pub.publish(odom_des);
     }
   }
 

c21d75a8ffdc562f45e551b4424f9bd42fd7749e
Giuseppe Loianno <loiannog@seas.upenn.edu>
2017-08-25 20:36:45 -0400

    fixing time to real time

--- include/trajectory/trajectory.h
+++ include/trajectory/trajectory.h
@@ -5,6 +5,7 @@ typedef std::vector< std::vector< std::vector<double> > > traj_type;
 
 // int loadTraj(const std::string &filename, traj_type &traj);
 ros::Time getMonotonicTime();
+ros::Time getRealTimeRos();
 
 class Trajectory
 {
